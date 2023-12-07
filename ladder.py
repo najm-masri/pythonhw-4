7b1ad7f84d18534dbfad49c6587359db859a89bb
najm-masri <115180086+najm-masri@users.noreply.github.com>
2023-12-06 22:07:19 -0800

    Update ladder.py

--- ladder.py
+++ ladder.py
@@ -1,1 +1,12 @@
+def my_steps(n):
+        
+        if(n<1 or n>25):
+            raise ValueError()
+        dp = {}
+        dp[1] = 1
+        dp[2] = 2
 
+        for i in range(3,n+1):
+            dp[i] = dp[i-1] + dp [i-2]
+    
+        return dp[n]
