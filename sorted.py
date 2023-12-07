c2aa8bf19c0c0ff822c6b5c2fcfcd8188dd79327
najm-masri <115180086+najm-masri@users.noreply.github.com>
2023-12-06 22:10:03 -0800

    Update sorted.py

--- sorted.py
+++ sorted.py
@@ -1,1 +1,8 @@
-
+def reverse_sort_dictionary(dict):
+    key = list(dict.keys())
+    key.sort()
+    ans = []
+    for i in range (len(key)-1,-1,-1):
+        ans.append((key[i],dict[key[i]][0]))
+        
+    return ans
