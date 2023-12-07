dc34c53f77d364931383c998647a451ec3efa314
najm-masri <115180086+najm-masri@users.noreply.github.com>
2023-12-06 22:04:13 -0800

    Update bug1.py

--- bug1.py
+++ bug1.py
@@ -1,1 +1,36 @@
+class Base:
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
 
+    def shape(self):
+        return "This is a"
+        
+    def draw(self):
+        return ""
+
+class Circle(Base):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size)
+
+    def draw(self):
+        return f"""
+({self.x}, {self.y})\n{self.size}
+         , - ~ ~ ~ - ,
+     , '               ' ,
+   ,                       ,
+  ,                         ,
+ ,                           ,
+ ,                           ,
+ ,                           ,
+  ,                         ,
+   ,                       ,
+     ,                  , '
+       ' - , _ _ _ ,  '
+               """
+
+
+
+    def shape(self):
+        return super().shape() +" circle"
