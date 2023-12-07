0f2285f1c1fd5d076cbbcd3782263dc3589e967d
najm-masri <115180086+najm-masri@users.noreply.github.com>
2023-12-06 22:08:23 -0800

    Update print_caps.py

--- print_caps.py
+++ print_caps.py
@@ -1,1 +1,12 @@
-
+def allcaps (f):
+    def inner():
+        val = f()
+        if(isinstance(val,list)):
+            for i in range(len(val)):
+                if(isinstance(val[i],str)):
+                    val[i] = val[i].upper()
+            return val
+        if(isinstance(val,str)):
+           return val.upper()
+   
+    return inner
